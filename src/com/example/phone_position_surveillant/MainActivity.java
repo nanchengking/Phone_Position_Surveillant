f624e159960805086fcb7349234c80d54eb414fe
Administrator <Administrator@YOS-01503051152>
2015-05-24 12:36:34 +0800

    however this version does not work well`~

--- src/com/example/phone_position_surveillant/MainActivity.java
+++ src/com/example/phone_position_surveillant/MainActivity.java
@@ -1,34 +1,109 @@
 package com.example.phone_position_surveillant;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private TextView positionView;
+	private LocationManager locationManager;
+	private String provider;
+	List<String> providerList;
+ 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		positionView = (TextView) findViewById(R.id.location_view);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		providerList = locationManager.getProviders(true);
+
+		if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+			provider = LocationManager.GPS_PROVIDER;
+		} else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+			provider = LocationManager.NETWORK_PROVIDER;
+		} else {
+			Toast.makeText(this, "No Location Provider to use",
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
+		Location location = locationManager.getLastKnownLocation(provider);
+		
+		if (location != null) {
+			showLocation(location);
+		}
+		
+		locationManager.requestLocationUpdates(provider, 500, 1,
+				locationListener);
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (locationManager != null) {
+			locationManager.removeUpdates(locationListener);
+		}
+	}
+
+	private LocationListener locationListener = new LocationListener() {
+
+		@Override
+		public void onLocationChanged(Location location) {
+			// 位置发生了改变
+			showLocation(location);
+
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+
+		}
+	};
+
+	private void showLocation(Location location) {
+		String currentPosition = "纬度是：" + location.getLatitude() + "\n"
+				+ "经度是：" + location.getLongitude();
+		positionView.setText(currentPosition);
+
+	}
+
 }
