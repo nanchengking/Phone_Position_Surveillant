7607ba8773f8401903f48a2cbd0ddf7102d09cdb
Administrator <Administrator@YOS-01503051152>
2015-05-24 19:20:40 +0800

    做了一个服务，加入了绑定机制，binder，但是还没有效果，马勒个蛋

--- src/com/example/phone_position_surveillant/MainActivity.java
+++ src/com/example/phone_position_surveillant/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.phone_position_surveillant;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,39 +24,67 @@ public class MainActivity extends Activity {
 	private LocationManager locationManager;
 	private String provider;
 	List<String> providerList;
- 
+	private MoveDetectiveService.AccelatorBinder mBinder;
+	private boolean isMoved;
+	float[] mAccelatorValues=new float[3];
+	private ServiceConnection connection = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			mBinder = (MoveDetectiveService.AccelatorBinder) service;
+			isMoved = mBinder.isMoved();
+			mAccelatorValues = mBinder.accelatorValues;
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		/**
+		 * 立马启动服务
+		 */
+		Intent startIntent = new Intent(this, MoveDetectiveService.class);
+		startService(startIntent);
+		/**
+		 * 绑定服务
+		 */
+		Intent bindIntent = new Intent(this, MoveDetectiveService.class);
+		bindService(bindIntent, connection, BIND_AUTO_CREATE);
+		
+		//显示经纬度和加速度的texView
 		positionView = (TextView) findViewById(R.id.location_view);
 		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		providerList = locationManager.getProviders(true);
-
-		if (providerList.contains(LocationManager.GPS_PROVIDER)) {
-			provider = LocationManager.GPS_PROVIDER;
-		} else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+		/**
+		 * 优先使用网络定位器
+		 */
+		if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
 			provider = LocationManager.NETWORK_PROVIDER;
+		} else if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+			provider = LocationManager.GPS_PROVIDER;
 		} else {
 			Toast.makeText(this, "No Location Provider to use",
 					Toast.LENGTH_SHORT).show();
 			return;
 		}
-		Log.d("Test", provider.toString()+01);
+		
 		Location location = locationManager.getLastKnownLocation(provider);
-		locationManager.requestLocationUpdates(provider, 1000,1,
+		locationManager.requestLocationUpdates(provider, 2000,2,
 				locationListener);
 		if (location != null) {
 			showLocation(location);
-			Log.d("Test", location.toString()+03);
 		}
-		Log.d("Test", locationListener.toString()+02);
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
@@ -60,8 +92,16 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch (id) {
+		case R.id.star_service:
+			
+			break;
+		case R.id.bind:
+			
+			break;
+		case R.id.unbind:
+			unbindService(connection);
+			break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
@@ -72,6 +112,10 @@ public class MainActivity extends Activity {
 		if (locationManager != null) {
 			locationManager.removeUpdates(locationListener);
 		}
+		unbindService(connection);
+		Intent stopService = new Intent(this, MoveDetectiveService.class);
+		stopService(stopService);
+
 	}
 
 	private LocationListener locationListener = new LocationListener() {
@@ -80,7 +124,7 @@ public class MainActivity extends Activity {
 		public void onLocationChanged(Location location) {
 			// 位置发生了改变
 			showLocation(location);
-			Log.d("Test", location.toString()+03);
+			Log.d("Test", location.toString() + 03);
 
 		}
 
@@ -102,11 +146,12 @@ public class MainActivity extends Activity {
 
 	private void showLocation(Location location) {
 		String currentPosition = "纬度是：" + location.getLatitude() + "\n"
-				+ "经度是：" + location.getLongitude();
+				+ "经度是：" + location.getLongitude() + mAccelatorValues[0] + "\n"
+				+ mAccelatorValues[1] + "\n" + mAccelatorValues[2];
 		positionView.setText(currentPosition);
-		
-		int i=0;
-		Log.d("Test","-"+(i++)+ location.toString());
+
+		int i = 0;
+		Log.d("Test", "-" + (i++) + location.toString());
 
 	}
 
--- src/com/example/phone_position_surveillant/MoveDetectiveService.java
+++ src/com/example/phone_position_surveillant/MoveDetectiveService.java
@@ -7,60 +7,104 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
-/**sensorManager  传感器管理
- * listener  传感器监听
+/**
+ * sensorManager 传感器管理 listener 传感器监听
+ * 
  * @author Administrator
  *
  */
-public class MoveDetectiveService extends Service{
-	
+public class MoveDetectiveService extends Service {
+
+	boolean isMoveed = false;
+	float xValue;
+	float yValue;
+	float zValue;
 	private SensorManager sensorManager;
+	// 一个用于绑定的binder
+	private AccelatorBinder mBinder = new AccelatorBinder();
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		return null;
+		return mBinder;
 	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
-		
-		Sensor sensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		
-		
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+		Sensor sensor = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		// 注册传感器
+		sensorManager.registerListener(listener, sensor,
+				sensorManager.SENSOR_DELAY_NORMAL);
+
 	}
 
 	@Override
 	public void onDestroy() {
-		
 		super.onDestroy();
+		if (sensorManager != null) {
+			sensorManager.unregisterListener(listener);
+		}
 	}
-	
 
-	private SensorEventListener listener=new SensorEventListener() {
-		
+	private SensorEventListener listener = new SensorEventListener() {
+
 		@Override
 		public void onSensorChanged(SensorEvent event) {
-		  //得到三个方向的加速度
-		   float xValue=Math.abs(event.values[0]);
-		   float yValue=Math.abs(event.values[1]);
-		   float zValue=Math.abs(event.values[2]);	
-		   
-		   if(xValue>15||yValue>15||zValue>16){
-			   //认为手机出现了晃动
-			   Toast.makeText(MoveDetectiveService.this, "手机被移动", Toast.LENGTH_SHORT);
-		   }		
+			// 得到三个方向的加速度
+			float mxValue = Math.abs(event.values[0]);
+			float myValue = Math.abs(event.values[1]);
+			float mzValue = Math.abs(event.values[2]);
+			xValue = mxValue;
+			yValue = myValue;
+			zValue = mzValue;
+
+			if (mxValue > 15 || myValue > 15 || mzValue > 16) {
+				// 认为手机出现了晃动
+				Toast.makeText(MoveDetectiveService.this, "手机被移动",
+						Toast.LENGTH_SHORT).show();
+				isMoveed = true;
+				int i = 0;
+				Log.d("Test", "--" + (i++) + "\n" + xValue + "\n" + yValue
+						+ "\n" + zValue);
+
+			}
 		}
-		
+
 		@Override
 		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			
-			
+
 		}
 	};
+
+	/**
+	 * 一个内部类，binder，用来在活动能够和服务之间通信，能够通过getAccelatorMeaasge（）来得到一个加速度数组
+	 * 
+	 * @author Administrator
+	 *
+	 */
+	class AccelatorBinder extends Binder {
+		private boolean misMoveed;
+		float[] accelatorValues = new float[3];
+
+		public AccelatorBinder() {
+			super();
+			misMoveed = isMoveed;
+			accelatorValues[0] = xValue;
+			accelatorValues[1] = yValue;
+			accelatorValues[2] = zValue;
+		}
+
+		public boolean isMoved() {
+			return misMoveed;
+		}
+	}
 
 }
